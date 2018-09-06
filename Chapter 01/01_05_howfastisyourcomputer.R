82b856a832456630217331aa7d5519154547ac49
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-09-05 19:20:37 -0700

    add upload_results and create_bundle

--- Chapter 01/01_05_howfastisyourcomputer.R
+++ Chapter 01/01_05_howfastisyourcomputer.R
@@ -14,6 +14,8 @@ get_cpu() # details about this processor
 # this creates your benchmark and rank
 bm_results <- benchmark_std() #generates a benchmark to compare
 plot(bm_results)
+upload_results(bm_results)
+create_bundle(bm_results, filename = "results.rds")
 
 # benchmarks for various machines
 # https://jumpingrivers.shinyapps.io/benchmarkme/ 
